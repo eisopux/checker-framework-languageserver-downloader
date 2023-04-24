7ccffcb6038794b89d25fdaf37dcbf078229b586
Werner Dietl <wdietl@gmail.com>
2023-04-24 10:30:28 -0400

    Update to Spotless 6.18.0 (#170)

--- src/main/java/org/checkerframework/languageserver/BaseDownloader.java
+++ src/main/java/org/checkerframework/languageserver/BaseDownloader.java
@@ -3,6 +3,11 @@ package org.checkerframework.languageserver;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
+
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -10,9 +15,6 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.io.IOUtils;
 
 public abstract class BaseDownloader {
     protected final String org;
