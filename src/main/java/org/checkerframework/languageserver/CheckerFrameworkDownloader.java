1e004cfb50d63d1ba5bd4ae760f6f21118bf88b3
Werner Dietl <wdietl@gmail.com>
2023-06-05 09:23:01 -0400

    Update to spotless 6.19, keeping existing import order (#176)

--- src/main/java/org/checkerframework/languageserver/CheckerFrameworkDownloader.java
+++ src/main/java/org/checkerframework/languageserver/CheckerFrameworkDownloader.java
@@ -1,12 +1,12 @@
 package org.checkerframework.languageserver;
 
-import net.lingala.zip4j.ZipFile;
-
 import org.apache.commons.io.FilenameUtils;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
+
+import net.lingala.zip4j.ZipFile;
 
 public class CheckerFrameworkDownloader extends BaseDownloader {
     public CheckerFrameworkDownloader(String org, String repo, File folder) {
